16f99715bf7e7d4d46fc91edfd0338096dde03dd
jacobemerick <jpemeric@gmail.com>
2016-07-27 16:58:33 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#logical-xor
+Exclusive or, which returns true only when the inputs differ.
---
+++ test.js
@@ -0,0 +1,6 @@
+const xor = require('index');
+
+console.log(xor(true, true));
+console.log(xor(true, false));
+console.log(xor(false, true));
+console.log(xor(false, false));
