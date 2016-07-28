a9a3f9269571e89f540f2e8a881de5ca9a0ddad1
jacobemerick <jpemeric@gmail.com>
2016-07-27 20:24:53 -0600

    Multi-dimensional xor

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-module.exports = (a, b) => ((!a && b) || (a && !b));
+/**
+ * Two-parameter xor function
+ */
+function simpleXor(a, b) {
+  return ((!a && b) || (a && !b));
+}
+
+/**
+ * Multi-parameter xor function
+ */
+function multiXor(parameters) {
+  let result = parameters.shift();
+
+  while (parameters.length) {
+    result = simpleXor(result, parameters.shift());
+  }
+
+  return result;
+}
+
+/**
+ * Accessible export
+ */
+module.exports = function exposedXor() {
+  let parameters = Object.keys(arguments).map(key => arguments[key]);
+
+  if (parameters.length < 1) {
+    throw new Error('Must pass in at least one argument');
+  }
+
+  if (parameters.length === 1) {
+    return parameters.shift();
+  }
+
+  return multiXor(parameters);
+};
